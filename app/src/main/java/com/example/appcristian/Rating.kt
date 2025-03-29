722025f84b9579701697ff66f717e4c321b21548
CristianDCM <cristianzdmc14@gmail.com>
2025-03-28 21:22:15 -0500

    Diseño de un RecyclerView a nivel vista en Android Studio con XML.

---
+++ app/src/main/java/com/example/appcristian/Rating.kt
@@ -0,0 +1,4 @@
+package com.example.appcristian
+
+data class Rating(val average: String,
+                  val reviews: String)
---
+++ app/src/main/java/com/example/appcristian/Wine.kt
@@ -0,0 +1,9 @@
+package com.example.appcristian
+
+data class Wine(val winery : String,
+                val wine : String,
+                val rating : Rating,
+                val location : String,
+                val image : String,
+                val id : Int)
+
